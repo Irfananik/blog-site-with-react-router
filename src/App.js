79aebce047eec125411c3de87d6a9feaa06529f9
Golam Morshed Bhuiyan <69519988+Irfananik@users.noreply.github.com>
2022-04-03 14:41:19 +0600

    blog details section done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Components/About/About';
+import BlogDetails from './Components/BlogDetails/BlogDetails';
 import Contact from './Components/Contact/Contact';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
@@ -13,7 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/home" element={<Home/>}></Route>
-        <Route pathe="/blog/:id"></Route>
+        <Route path="/blog/:id" element={<BlogDetails/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/contact" element={<Contact/>}></Route>
         <Route path="*" element={<NotFound/>}></Route>
---
+++ src/Components/BlogDetails/BlogDetails.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+const BlogDetails = () => {
+    const { id } = useParams()
+    const navigate = useNavigate()
+    const [blog,setBlog] = useState([])
+
+    useEffect(() => {
+        fetch(`https://retro-tech-talks.herokuapp.com/getBlog/${id}`)
+        .then(res => res.json())
+        .then(data => setBlog(data))
+    },[id])
+    return (
+        <div className="bg-slate-200 mt-16 rounded-lg p-6">
+            {id}
+            <h1 className="font-bold text-xl text-slate-500">Titile: {blog.title}</h1>
+            <div className="flex justify-center mt-6 rounded-lg">
+                <img className="w-40 rounded-lg" src={blog.imageURL} alt="" />
+            </div>
+            <div className="mt-6 ">
+                <p className="text-slate-600">{blog.blog}</p>
+            </div>
+            <div>
+                <button onClick={() => navigate("/home")} className="rounded-full bg-slate-500 p-3 mt-6">
+                    back to home
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default BlogDetails;
